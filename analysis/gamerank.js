215fdda04e2e7dd230ce3ddab26060197b5be5b6
Zifei Shan <shanzifei@pku.edu.cn>
2012-08-09 15:09:29 +0800

    init gamerank

---
+++ analysis/gamerank.js
@@ -0,0 +1,49 @@
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(50)
+    .size([width, height]);
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("../data/test.json", LayoutJson);
+
+function LayoutJson(json) {
+  force
+      .nodes(json.match[0].nodes)
+      .links(json.match[0].links)
+      .start();
+
+  var link = svg.selectAll("line.link")
+      .data(json.match[0].links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.selectAll("circle.node")
+      .data(json.match[0].nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.group); })
+      .call(force.drag);
+
+  node.append("title")
+      .text(function(d) { return d.name; });
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+}
