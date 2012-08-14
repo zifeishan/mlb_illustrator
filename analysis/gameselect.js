3e6eba50a7350686b4ff5d765a9f69784de74179
Zifei Shan <shanzifei@pku.edu.cn>
2012-08-14 16:40:49 +0800

    Update team list logic

--- analysis/gameselect.js
+++ analysis/gameselect.js
@@ -1,4 +1,5 @@
 var gameyear = d3.select("#selyear");
+var gameindex = d3.select("#selteam");
 
 // for(var i = 1940; i < 2012; i++) {
 for(var i = 2010; i < 2012; i++) {
@@ -6,33 +7,10 @@ for(var i = 2010; i < 2012; i++) {
     .append("option")
     .attr("value", i).text(i);      
 }
-
-var gameindex = d3.select("#selteam");
-
-d3.json("../data/teamcode.json",function(json){
-
-  for(var sub in json.teamcode)
-  {
-    // console.log(json.teamcode[sub]);
-    var pair = json.teamcode[sub];
-    for(var key in pair)
-      // if(pair[key].end == "0")
-    {
-      var v = pair[key];
-        gameindex
-          .append("option")
-          .attr("value", key)
-          .text(key+" ("
-            + (v.start != "0" ? v.start : "Unkn")
-            + "--"
-            + (v.end != "0" ? v.end : "Now." )
-            + ") "
-            + pair[key].name);
-     }
-  }
-});
+RefreshTeams();
 
 gameyear.on("change", RefreshLayout);
+gameyear.on("change", RefreshTeams);
 gameindex.on("change", RefreshLayout);
 
 function RefreshLayout() {
@@ -52,3 +30,40 @@ function RefreshLayout() {
 
 }
 
+
+function RefreshTeams()
+{
+  var TeamCodes = d3.json("../data/teamcode.json",function(json){
+
+    var y = document.getElementById("selyear");
+    var year = y.options[y.selectedIndex].value;
+
+    for(var sub in json.teamcode)
+    {
+      var pair = json.teamcode[sub];
+
+      for(var key in pair)
+        // if(pair[key].end == "0")
+      {
+        //get some teams out
+        if(pair[key].end != 0 && pair[key].end < year)
+        {
+          // console.log(pair[key].end);
+          continue;
+        }
+        if(pair[key].begin != 0 && pair[key].begin < year)
+          continue;
+        var v = pair[key];
+          gameindex
+            .append("option")
+            .attr("value", key)
+            .text(key+" ("
+              + (v.start != "0" ? v.start : "Unkn")
+              + "--"
+              + (v.end != "0" ? v.end : "Now." )
+              + ") "
+              + pair[key].name);
+       }
+    }
+  });
+}
